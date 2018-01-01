9f9725415919d5ee496d192e526f451526739d31
Phil J. Laszkowicz <phil@fillip.pro>
2018-01-01 12:59:24 +0200

    TypeScript build setup.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        ostaa: "./src/index.ts",
+    },
+    module: {
+        rules: [
+            {
+                exclude: '/node_modules/',
+                loader: 'ts-loader',
+                test: /\.tsx?$/
+            }
+        ]
+    },
+    plugins: [
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '.build/assets/scripts/js')
+    },
+    resolve: {
+        extensions: [".ts", ".json", ".js"]
+    }
+}
